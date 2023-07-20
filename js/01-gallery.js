942a318157c43af3b08216a5ca739c7aaf0f6986
Vira Smulska <95082786+Vera-Smulskaya@users.noreply.github.com>
2023-07-20 19:18:02 +0300

    fix problem with show galery in browser

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+console.log("galleryList:", galleryList);
+const galleryListItems = createGalleryListHTML(galleryItems);
+console.log("galleryListItems:", galleryListItems);
+galleryList.insertAdjacentHTML("beforeend", galleryListItems);
+
+function createGalleryListHTML(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="large-image.jpg"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join("");
+}
